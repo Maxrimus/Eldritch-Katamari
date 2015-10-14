68a3423b1ff92c348514fb570c1577cb3474a1e8
Sadiki <sxs6590@rit.edu>
2015-10-14 18:21:42 -0400

    (Prototype Alpha)^2 Start Menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public bool isStartGame;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Performs an action when the mouse is up
+	void OnMouseUp(){
+		// if true then start
+		if (isStartGame) {
+			Application.LoadLevel(1);
+		}
+	}
+}
